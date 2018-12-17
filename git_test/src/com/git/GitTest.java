f387596d3e297b7cd4f82a8267f3565be27fe108
xioabai <xiaobai@itcast.cn>
2018-12-17 20:06:28 +0800

    提交git_test工程

---
+++ git_test/src/com/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git.....");
+        System.out.println("git.....");
+        System.out.println("git.....");
+        System.out.println("git.....");
+        System.out.println("git.....");
+        System.out.println("git.....");
+        System.out.println("git.....");
+        System.out.println("git.....");
+
+    }
+}
